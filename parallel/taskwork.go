ae0c3a1bd4246b77e5a7dc3c9e37f79ecc260e20
FranciscoCardoso913 <franciscocardoso.3003@gmail.com>
2024-10-05 18:57:19 +0100

    Parallel Pipeline

---
+++ parallel/taskwork.go
@@ -0,0 +1,45 @@
+//
+// Task Wroker
+// Connects PULL socket to tcp://localhost:5557
+// Collects workloads from ventilator via that socket
+// Connects PUSH socket to tcp://localhost:5558
+// Sends results to sink via that socket 
+// 
+package main
+
+import (
+	"fmt"
+	zmq "github.com/pebbe/zmq4"
+	
+	"strconv"
+	"time"
+)
+
+func main() {
+	context, _ := zmq.NewContext()
+	defer context.Term()
+
+	//  Socket to receive messages on
+	receiver, _ := context.NewSocket(zmq.PULL)
+	defer receiver.Close()
+	receiver.Connect("tcp://localhost:5557")
+
+	//  Socket to send messages to task sink
+	sender, _ := context.NewSocket(zmq.PUSH)
+	defer sender.Close()
+	sender.Connect("tcp://localhost:5558")
+
+	//  Process tasks forever
+	for {
+		msgbytes, _ := receiver.Recv(0)
+		fmt.Printf("%s.\n", string(msgbytes))
+
+		//  Do the work
+		msec, _ := strconv.ParseInt(string(msgbytes), 10, 64)
+		time.Sleep(time.Duration(msec) * 1e6)
+
+		//  Send results to sink
+		sender.Send("", 0)
+
+	}
+}
